f9f417ddf92653fd8c25027203a05e1b14ea7d5c
Christophe.Ambroise <christophe.ambroise@gmail.com>
2026-06-05 13:56:18 +0200

    pynem: bump version to 0.2.0 (PPanGGOLiN pipeline)

--- pynem/src/pynem/__init__.py
+++ pynem/src/pynem/__init__.py
@@ -4,6 +4,6 @@ from .core import NEM
 from . import io, viz, models, spatial, metrics, ppanggolin
 from .ppanggolin import partition_pangenome
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["NEM", "io", "viz", "models", "spatial", "metrics",
            "ppanggolin", "partition_pangenome"]
